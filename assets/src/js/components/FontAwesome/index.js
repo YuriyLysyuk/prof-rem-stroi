030e0c3797bb8b6112db10907d542422a165806f
YuriyLysyuk <work@yuriylysyuk.ru>
2023-03-23 19:11:41 +0300

    в футер добавлены соц кнопки

--- assets/src/js/components/FontAwesome/index.js
+++ assets/src/js/components/FontAwesome/index.js
@@ -2,13 +2,30 @@ import { library, dom } from '@fortawesome/fontawesome-svg-core';
 
 // Brands
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons/faWhatsapp';
+import { faVk } from '@fortawesome/free-brands-svg-icons/faVk';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
+import { faOdnoklassniki } from '@fortawesome/free-brands-svg-icons/faOdnoklassniki';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons/faYoutube';
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons/faFacebookSquare';
+import { faTiktok } from '@fortawesome/free-brands-svg-icons/faTiktok';
 
 // Solid
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
 import { faPhone } from '@fortawesome/free-solid-svg-icons/faPhone';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons/faLocationDot';
 
-const icons = [faWhatsapp, faEnvelope, faPhone, faLocationDot];
+const icons = [
+  faWhatsapp,
+  faVk,
+  faInstagram,
+  faOdnoklassniki,
+  faYoutube,
+  faFacebookSquare,
+  faTiktok,
+  faEnvelope,
+  faPhone,
+  faLocationDot,
+];
 
 library.add(icons);
 dom.watch();
