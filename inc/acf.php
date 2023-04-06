815e8b10200809ea41f3ef593276f70d1f69477a
YuriyLysyuk <work@yuriylysyuk.ru>
2023-04-06 18:06:58 +0500

    добавлена страница общих настроек секций

--- inc/acf.php
+++ inc/acf.php
@@ -16,4 +16,10 @@ if ( function_exists( 'acf_add_options_page' ) ) {
 		'capability' => 'edit_posts',
 		'redirect' => false
 	) );
+
+	acf_add_options_sub_page( array(
+		'page_title' => 'Секции',
+		'menu_title' => 'Секции',
+		'parent_slug' => 'theme-settings',
+	) );
 }
