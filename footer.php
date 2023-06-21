a25dc8fc2eea2765c56de7141e84d38f0f33ba1e
YuriyLysyuk <work@yuriylysyuk.ru>
2023-06-21 13:38:31 +0300

    модалка сметы перемещена в шаблон сметы

--- footer.php
+++ footer.php
@@ -101,48 +101,6 @@ $socialsList = get_field( 'socials_list', 'option' );
 	</div>
 </footer>
 
-<section class="modal hidden" data-modal="smeta">
-	<div class="modal__header">
-		<h3 class="modal__title">Отправка сметы</h3>
-
-		<button class="modal__close" type="button" data-modal-close="smeta">
-			x
-		</button>
-	</div>
-
-	<div class="modal__body">
-		<form class="js-form form form_vertical" enctype="multipart/form-data">
-			<label class="form__control">
-				<i class="fa-solid fa-user"></i>
-
-				<input type="text" name="name" placeholder="Ваше имя" />
-			</label>
-
-			<label class="form__control">
-				<i class="fa-solid fa-phone"></i>
-
-				<input class="js-phone-mask" type="text" name="tel" placeholder="Ваш номер телефона" required />
-			</label>
-
-			<label class="form__control form__control_file">
-				<i class="fa-solid fa-file"></i>
-
-				<input type="file" name="file" placeholder="Смета" />
-			</label>
-
-			<label class="form__control">
-				<i class="fa-solid fa-message"></i>
-
-				<textarea name="comment" rows="5" placeholder="Комментарий"></textarea>
-			</label>
-
-			<button class="btn btn_small" type="submit">Отправить</button>
-
-			<div class="js-error error error_hidden"></div>
-		</form>
-	</div>
-</section>
-
 <!-- Оверлей для модалок, всегда один -->
 <div class="js-overlay overlay hidden"></div>
 
--- templates/sections/smeta.php
+++ templates/sections/smeta.php
@@ -34,3 +34,45 @@ $buttonModalId = get_field( 'smeta_button-modal', 'option' );
 		</div>
 	</div>
 </section>
+
+<section class="modal hidden" data-modal="smeta">
+	<div class="modal__header">
+		<h3 class="modal__title">Отправка сметы</h3>
+
+		<button class="modal__close" type="button" data-modal-close="smeta">
+			x
+		</button>
+	</div>
+
+	<div class="modal__body">
+		<form class="js-form form form_vertical" enctype="multipart/form-data">
+			<label class="form__control">
+				<i class="fa-solid fa-user"></i>
+
+				<input type="text" name="name" placeholder="Ваше имя" />
+			</label>
+
+			<label class="form__control">
+				<i class="fa-solid fa-phone"></i>
+
+				<input class="js-phone-mask" type="text" name="tel" placeholder="Ваш номер телефона" required />
+			</label>
+
+			<label class="form__control form__control_file">
+				<i class="fa-solid fa-file"></i>
+
+				<input type="file" name="file" placeholder="Смета" />
+			</label>
+
+			<label class="form__control">
+				<i class="fa-solid fa-message"></i>
+
+				<textarea name="comment" rows="5" placeholder="Комментарий"></textarea>
+			</label>
+
+			<button class="btn btn_small" type="submit">Отправить</button>
+
+			<div class="js-error error error_hidden"></div>
+		</form>
+	</div>
+</section>
