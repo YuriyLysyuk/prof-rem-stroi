3401eefbd73a0d1b8e41d8a691cafe5588bc39e4
YuriyLysyuk <work@yuriylysyuk.ru>
2023-07-03 17:19:28 +0300

    удалены настройки bitrix

--- integration/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Настройки
-define( 'BITRIX24_WEBHOOK_URL', 'https://profremstroy.bitrix24.ru/rest/223/us0dl4eph8cohlx8/' );
-define( 'BITRIX24_LEAD_TITLE', 'Заявка с сайта' );
-define( 'BITRIX24_LEAD_SOURCE_ID', 'UC_CWQYE8' );
